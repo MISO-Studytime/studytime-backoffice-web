99183fc54d4176ece62041b74e86a85fdfefe77f
Miguel Tovar <miguel@miguel.com>
2024-03-11 22:36:02 -0500

    maquetando pantalla de signin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SignInComponent } from './SignIn/SignIn.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: SignInComponent, pathMatch: 'full' },
+];
+
