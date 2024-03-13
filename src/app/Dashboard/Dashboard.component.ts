ef08ff11c16d76906d3ce7797d84b7e555d7765b
Miguel Tovar <miguel@miguel.com>
2024-03-13 00:51:02 -0500

    ajustando el dashboard

--- src/app/Dashboard/Dashboard.component.ts
+++ src/app/Dashboard/Dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild, AfterViewInit } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -7,6 +7,28 @@ import {MatButtonModule} from '@angular/material/button';
 import { Router } from '@angular/router';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import {MatSort, MatSortModule, SortDirection} from '@angular/material/sort';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {DatePipe} from '@angular/common';
+import {MatTableModule} from '@angular/material/table';
+
+
+
+
+export interface UserElement {
+  user: string;
+  userType: string;
+  email: string;
+}
+
+const ELEMENT_DATA: UserElement[] = [
+  {user:'Randy',userType:'Usuario study time',email:'randy.williams@yahoo.es'},
+  {user:'Jhon',userType:'Usuario study time',email:'randy.williams@yahoo.es'},
+  {user:'Erika',userType:'Usuario study time',email:'randy.williams@yahoo.es'},
+  {user:'Juan',userType:'Usuario study time',email:'randy.williams@yahoo.es'},
+  
+];
 
 @Component({
   selector: 'app-Dashboard',
@@ -14,10 +36,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   styleUrls: ['./Dashboard.component.css'],
   standalone:true,
   imports: [
-    FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule,MatGridListModule,MatButtonModule,MatToolbarModule,MatButtonModule
+    FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule,MatGridListModule,MatButtonModule,MatToolbarModule,MatButtonModule,
+    MatProgressSpinnerModule, MatTableModule, MatSortModule, MatPaginatorModule, DatePipe,MatSort,MatPaginator
    ]
 })
 export class DashboardComponent implements OnInit {
+  displayedColumns: string[] = ['user', 'userType', 'email'];
+  dataSource = ELEMENT_DATA;
   
   constructor(private router: Router) { }
 
