aa1321da3338078f22165d5d613f67eb73c7b476
Miguel Tovar <miguel@miguel.com>
2024-03-12 23:18:10 -0500

    recovery password finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { SignInComponent } from './SignIn/SignIn.component';
+import { RecoveryPasswordComponent } from './RecoveryPassword/RecoveryPassword.component';
 
 export const routes: Routes = [
     { path: '', component: SignInComponent, pathMatch: 'full' },
+    { path: 'recovery-pass', component: RecoveryPasswordComponent, pathMatch: 'full' },
+    { path: 'sign-in', component: SignInComponent, pathMatch: 'full' },
 ];
 
